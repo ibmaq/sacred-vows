aa9b9e46fcb1c28546323f06e1c580a11a17e7d8
ibrahim-agit <ibrahim.aginfotech@gmail.com>
2025-01-06 15:48:00 +0500

    update(whatsapp-number): updated whatsapp number

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import { ComingSoon } from "@/components/reusable/ComingSoon";
+import { Hero } from "@/components/reusable/Hero";
 
 export default function Home() {
   return (
     <>
-      {/* <Hero /> */}
-      <ComingSoon />
+      <Hero />
+      {/* <ComingSoon /> */}
     </>
   );
 }
--- components/WhatsApp.jsx
+++ components/WhatsApp.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from 'react-icons/fa';
 const WhatsappButton = () => {
   return (
     <a
-      href="https://wa.me/1234567890"
+      href="https://wa.me/+923472592564"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition"
